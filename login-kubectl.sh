16b4bea4f466249a0d844814f2f6b40c420d4160
Aarnav Tale <aarnav@tale.me>
2022-06-27 02:50:47 -0400

    fix: workaround github actions not decoding properly

--- login-kubectl.sh
+++ login-kubectl.sh
@@ -4,4 +4,5 @@ if [ ! -d "$HOME/.kube" ]; then
 	mkdir -p $HOME/.kube
 fi
 
-echo "$BASE64_KUBE_CONFIG" | base64 -d > $HOME/.kube/config
+echo "$BASE64_KUBE_CONFIG" $HOME/.kube/config.b64
+cat $HOME/.kube/config.b64 | base64 -d > $HOME/.kube/config
