6ac935ba0c063f79dc890b6b6962d5ed16234bfe
Aarnav Tale <aarnav@tale.me>
2023-01-25 10:07:45 -0500

    fix: run all actions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-/* eslint-disable unicorn/prefer-top-level-await */
 import { debug, getState, setFailed } from '@actions/core'
 import { setupKubeconfig } from 'login'
 import { installKubectl } from 'setup'
@@ -7,6 +6,7 @@ const post = Boolean(getState('isPost'))
 
 if (!post) {
 	debug('Running kubectl-action setup')
+	// eslint-disable-next-line no-async-promise-executor
 	new Promise(async () => {
 		await installKubectl()
 		debug('kubectl-action setup complete')
@@ -14,6 +14,7 @@ if (!post) {
 		await setupKubeconfig()
 		debug('kubectl-action kubeconfig setup complete')
 	})
+		// eslint-disable-next-line unicorn/prefer-top-level-await
 		.catch(error => {
 			setFailed('Failed to install kubectl (this is a bug in kubectl-action): ')
 			debug(JSON.stringify(error))
