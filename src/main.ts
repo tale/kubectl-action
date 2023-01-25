ff8bf47b42b1fd8eac8cfadbdb485fe946942914
Aarnav Tale <aarnav@tale.me>
2023-01-25 11:17:31 -0500

    feat: actually execute post action

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { debug, getState, setFailed } from '@actions/core'
 import { setupKubeconfig } from 'login'
 import { installKubectl } from 'setup'
+import { teardown } from 'teardown'
 
-const post = Boolean(getState('isPost'))
-
-if (!post) {
+if (getState('kubectl-path')) {
+	debug('Running post kubectl-action setup')
+	teardown()
+		// eslint-disable-next-line unicorn/prefer-top-level-await
+		.catch(error => {
+			setFailed('Failed to install kubectl (this is a bug in kubectl-action): ')
+			debug(JSON.stringify(error))
+		})
+} else {
 	debug('Running kubectl-action setup')
 	// eslint-disable-next-line no-async-promise-executor
 	new Promise(async () => {
