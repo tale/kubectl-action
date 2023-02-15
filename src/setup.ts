f08750dda0ef4b26900555796d462e8529ca2cea
Aarnav Tale <aarnavtale@icloud.com>
2023-02-15 13:46:46 -0500

    fix: set permission to 775 not 755

--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,6 @@
+import { execSync } from 'node:child_process'
 import { createHash, randomUUID } from 'node:crypto'
-import { mkdir, writeFile } from 'node:fs/promises'
+import { chmod, mkdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { env, stdout } from 'node:process'
 import { clearLine, cursorTo } from 'node:readline'
@@ -41,6 +42,8 @@ export async function installKubectl() {
 
 	console.log(`Installing kubectl to ${path}`)
 	await writeFile(join(path, 'kubectl'), kubectl)
+
+	execSync(`chmod +x ${join(path, 'kubectl')}`)
 	addPath(path)
 }
 
